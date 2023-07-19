d73ee76a1bf81d3dba4c9a2359ee82f15b8d17f8
Min <esspacetoM@gmail.com>
2023-07-20 00:14:57 +0900

    feat: 서버에 라우터 등록

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,21 @@
+import "./db";
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-const PORT = 4000;
+import globalRouter from "./routes/globalRouter";
+import userRouter from "./routes/userRouter";
+import postRouter from "./routes/postRouter";
 
 const app = express();
+const logger = morgan("dev");
 
-app.get("/", (req, res) => res.send("HELLO"));
+app.use(logger);
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => console.log("SERVER"));
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/post", postRouter);
+
+export default app;
