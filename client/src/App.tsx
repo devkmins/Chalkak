c3fca71f1f49a59af7d0ca128290edc7358d093a
Min <esspacetoM@gmail.com>
2023-07-25 21:58:43 +0900

    feat: react query 사용 및 Join 컴포넌트, Posts 컴포넌트 렌더링

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Posts from "./components/Posts";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Join from "./components/Join";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div>React</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Join></Join>
+      <Posts></Posts>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
