c6bd36adfbb4714c8150f7baa5c0b4c90fb0b0c4
Min <esspacetoM@gmail.com>
2023-07-25 22:03:33 +0900

    edit: postRouter의 라우트 파라미터 이름 변경 및 PATCH 요청 핸들러로 변경

--- server/routes/postRouter.ts
+++ server/routes/postRouter.ts
@@ -4,15 +4,18 @@ import {
   getUpload,
   postUpload,
   getPostEdit,
-  postPostEdit,
+  patchPostEdit,
   deletePost,
 } from "../controllers/postController";
 
 const postRouter = express.Router();
 
-postRouter.get("/:id([0-9a-f]{24})", watch);
+postRouter.get("/:pid([0-9a-f]{24})", watch);
 postRouter.route("/upload").get(getUpload).post(postUpload);
-postRouter.route("/:id([0-9a-f]{24})/edit").get(getPostEdit).post(postPostEdit);
-postRouter.route("/:id([0-9a-f]{24})/delete").delete(deletePost);
+postRouter
+  .route("/:pid([0-9a-f]{24})/edit")
+  .get(getPostEdit)
+  .patch(patchPostEdit);
+postRouter.route("/:pid([0-9a-f]{24})/delete").delete(deletePost);
 
 export default postRouter;
