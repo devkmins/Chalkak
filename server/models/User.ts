74baf40c168531347ed8fa682f66e394eb9bd686
Min <esspacetoM@gmail.com>
2023-07-25 22:17:15 +0900

    edit: User 스키마 수정 및 name 추가

--- server/models/User.ts
+++ server/models/User.ts
@@ -12,7 +12,7 @@ interface IUser {
 }
 
 const userSchema = new mongoose.Schema({
-  id: String,
+  name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
