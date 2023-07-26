dbb91a9ff2fb47b20d9b888b57a14c9ffb04654f
Min <esspacetoM@gmail.com>
2023-07-26 20:57:07 +0900

    feat: 라우팅을 위한 BrowserRouter 추가 및 Route 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import Posts from "./components/Posts";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Join from "./components/Join";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Join></Join>
-      <Posts></Posts>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Posts />}></Route>
+          <Route path="/join" element={<Join />}></Route>
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
