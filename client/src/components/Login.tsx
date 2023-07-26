6f884603fb87246b6283b5cf43961684602df691
Min <esspacetoM@gmail.com>
2023-07-26 23:54:27 +0900

    feat: 로그인 기능을 위한 Login 컴포넌트 생성

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    id: "",
+    password: "",
+  });
+
+  const [error, setError] = useState();
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const response = await axios
+      .post("http://localhost:4000/login", formData)
+      .then((response) => navigate("/"))
+      .catch((error) => setError(error.response.data.error));
+  };
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          name="id"
+          type="text"
+          placeholder="id"
+          value={formData.id}
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </form>
+    </>
+  );
+}
+
+export default Login;
