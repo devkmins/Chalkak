fe13aa8068f52645c49f7032304e0d9aa1af8503
Min <esspacetoM@gmail.com>
2023-07-26 21:15:57 +0900

    feat: globalController에 회원가입을 위한 postJoin 생성

--- server/controllers/globalController.ts
+++ server/controllers/globalController.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from "express";
 import Post from "../models/Post";
+import User from "../models/User";
 
 export const home = async (req: Request, res: Response) => {
   const posts = await Post.find({});
   return res.json(posts);
 };
 
-export const getJoin = (req: Request, res: Response) => res.send("Join");
+export const postJoin = async (req: Request, res: Response) => {
+  const { name, email, username, password } = req.body;
 
-export const postJoin = (req: Request, res: Response) => res.send("Join");
+  try {
+    const newUser = await User.create({
+      name,
+      email,
+      username,
+      password,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("404");
+  }
+};
 
 export const getLogin = (req: Request, res: Response) => res.send("Login");
 
