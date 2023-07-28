fe099aac75db112fe6b4221e5cb2424740c513e5
Min <esspacetoM@gmail.com>
2023-07-28 18:26:10 +0900

    edit: MongoDB와 연결되는 URL을 env 파일에 추가

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/chalkak", {});
+mongoose.connect(process.env.DB_URL as string, {});
 
 const db = mongoose.connection;
 
