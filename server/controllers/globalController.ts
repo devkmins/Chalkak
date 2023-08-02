c32956ae8822be1e310c10061f29f3ffaaf4774d
Min <esspacetoM@gmail.com>
2023-08-02 23:00:25 +0900

    feat: 로그인 기능, 로그인 판별, 세션 정보 전달 구현

--- server/controllers/globalController.ts
+++ server/controllers/globalController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Post from "../models/Post";
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import { CustomSessionData } from "../types/express";
 
 export const home = async (req: Request, res: Response) => {
   const posts = await Post.find({});
@@ -38,7 +39,16 @@ export const postJoin = async (req: Request, res: Response) => {
       .status(200)
       .json({ message: "회원가입이 성공적으로 완료되었습니다." });
   } catch (error) {
-    return res.status(400).json({ error: "회원가입에 실패했습니다." });
+    return res.status(400).send({ error: "회원가입에 실패했습니다." });
+  }
+};
+
+export const getLogin = (req: Request, res: Response) => {
+  if ((req.session as CustomSessionData).loggedIn) {
+    return res.send({
+      loggedIn: (req.session as CustomSessionData).loggedIn,
+      user: (req.session as CustomSessionData).user,
+    });
   }
 };
 
@@ -56,7 +66,18 @@ export const postLogin = async (req: Request, res: Response) => {
     return res.status(400).json({ error: "loginPasswordError" });
   }
 
-  return res.status(200).json({ message: "로그인이 완료되었습니다." });
+  const userSessionData = {
+    email: user.email,
+    name: user.name,
+    username: user.username,
+    socialOnly: user.socialOnly,
+    profileImage: user.profileImage,
+  };
+
+  (req.session as CustomSessionData).loggedIn = true;
+  (req.session as CustomSessionData).user = userSessionData;
+
+  return res.status(200).send("Login");
 };
 
 export const search = (req: Request, res: Response) => res.send("Search");
