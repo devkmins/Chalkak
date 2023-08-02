ec376600ed3f2f758f0e11d58729516fea23c479
Min <esspacetoM@gmail.com>
2023-08-02 22:57:20 +0900

    feat: getLogin 핸들러 추가

--- server/routes/globalRouter.ts
+++ server/routes/globalRouter.ts
@@ -4,13 +4,14 @@ import {
   search,
   postJoin,
   postLogin,
+  getLogin,
 } from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.route("/join").post(postJoin);
-globalRouter.route("/login").post(postLogin);
+globalRouter.route("/login").get(getLogin).post(postLogin);
 globalRouter.get("/search", search);
 
 export default globalRouter;
