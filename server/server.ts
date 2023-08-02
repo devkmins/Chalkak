e743c05e69b90eeda48fdbb3ae4846f63f597299
Min <esspacetoM@gmail.com>
2023-08-02 22:56:06 +0900

    feat: cors 설정 추가

--- server/server.ts
+++ server/server.ts
@@ -18,7 +18,8 @@ const logger = morgan("dev");
 app.use(logger);
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
 );
