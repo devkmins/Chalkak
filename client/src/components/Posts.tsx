244460170d3d927b1e78c72ab7f50d67e7e79237
Min <esspacetoM@gmail.com>
2023-08-02 23:01:03 +0900

    feat: 로그인 상태 유지 구현

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -4,20 +4,44 @@ import CreatePost from "./CreatePost";
 import EditPost from "./EditPost";
 import DeletePost from "./DeletePost";
 import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { loggedInState, sessionState } from "../atoms/atoms";
+import { useEffect } from "react";
 
 function Posts() {
   const { data, isLoading, isError } = useQuery("getData", () =>
     axios.get("http://localhost:4000/").then((response) => response.data)
   );
 
+  const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
+  const [sessionData, setSessionData] = useRecoilState(sessionState);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/login", { withCredentials: true })
+      .then((response) => {
+        setLoggedIn(response.data.loggedIn);
+        setSessionData(response.data.user);
+      });
+  }, []);
+
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error occurred.</div>;
 
   return (
     <div>
-      <Link to={"/join"}>Join</Link>
-      <Link to={"/login"}>Login</Link>
-      <CreatePost />
+      {loggedIn ? (
+        <>
+          <Link to={"/user/logout"}>Logout</Link>
+          <CreatePost />
+        </>
+      ) : (
+        <>
+          <Link to={"/join"}>Join</Link>
+          <Link to={"/login"}>Login</Link>
+        </>
+      )}
+
       {data.map((post: any) => (
         <div
           key={post._id}
