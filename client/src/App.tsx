20c9dba571d94a69d2847fc87ec656946ae63a10
Min <esspacetoM@gmail.com>
2023-08-06 23:39:33 +0900

    edit: 로그인 여부에 따라 라우트 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 import Posts from "./components/Posts";
-import { QueryClient, QueryClientProvider } from "react-query";
 import Join from "./components/Join";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import { useRecoilValue } from "recoil";
 import Logout from "./components/Logout";
-import { loggedInState } from "./atoms/atoms";
-
-const queryClient = new QueryClient();
+import { loggedInState } from "./atoms";
+import useGetLoginData from "./hooks/useGetLoginData";
 
 function App() {
   const loggedIn = useRecoilValue(loggedInState);
+  useGetLoginData();
 
   return (
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/" element={<Posts />} />
-          {loggedIn ? (
-            <>
-              <Route path="/user/logout" element={<Logout />} />
-            </>
-          ) : (
-            <>
-              <Route path="/join" element={<Join />} />
-              <Route path="/login" element={<Login />} />
-            </>
-          )}
-        </Routes>
-      </QueryClientProvider>
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        {loggedIn ? (
+          <>
+            <Route path="/join" element={<Navigate to="/" />} />
+            <Route path="/login" element={<Navigate to="/" />} />
+            <Route path="/user/logout" element={<Logout />} />
+          </>
+        ) : (
+          <>
+            <Route path="/join" element={<Join />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/user/logout" element={<Navigate to="/login" />} />
+          </>
+        )}
+      </Routes>
     </BrowserRouter>
   );
 }
