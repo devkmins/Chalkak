8da5e2a83ec67ade1078bdc1c468cb0e1f34a41d
Min <esspacetoM@gmail.com>
2023-08-12 21:47:08 +0900

    remove: changePassword, deleteAccount 핸들러 제거

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -31,9 +31,3 @@ export const logout = (req: Request, res: Response) => {
 export const getUserEdit = (req: Request, res: Response) => res.send("Edit");
 
 export const postUserEdit = (req: Request, res: Response) => res.send("Edit");
-
-export const changePassword = (req: Request, res: Response) =>
-  res.send("Change");
-
-export const deleteAccount = (req: Request, res: Response) =>
-  res.send("Delete Account");
