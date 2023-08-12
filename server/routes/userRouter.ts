3cb45b5e013f88d761a9be0fcee2efe9e5aaeafb
Min <esspacetoM@gmail.com>
2023-08-12 23:19:08 +0900

    remove: deleteAccount, changePassword 라우트 제거

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -4,8 +4,6 @@ import {
   logout,
   getUserEdit,
   postUserEdit,
-  deleteAccount,
-  changePassword,
 } from "../controllers/userController";
 import { protectorMiddleware } from "../middlewares";
 
@@ -13,8 +11,6 @@ const userRouter = express.Router();
 
 userRouter.post("/logout", logout);
 userRouter.route("/edit").get(getUserEdit).post(postUserEdit);
-userRouter.post("/change-password", changePassword);
-userRouter.route("/close").delete(deleteAccount);
 userRouter.get("/:uid([a-zA-Z0-9]+)", see);
 
 export default userRouter;
