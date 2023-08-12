a5ec0f295ee1eab4cf0e0d8afd50ead133fc4e74
Min <esspacetoM@gmail.com>
2023-08-12 23:47:54 +0900

    feat: 유저의 프로필에 접속하였을 때, 해당 유저의 username과 세션 쿠키에 저장된 username이 동일한 경우, 계정 설정 페이지로 이동하는 Link 렌더링

--- client/src/components/UserPage.tsx
+++ client/src/components/UserPage.tsx
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { sessionState } from "../atoms";
+import { Link } from "react-router-dom";
 
 function UserPage() {
   const location = useLocation();
-  const postOwner = location.state;
+  const username = location.state;
+
+  const sessionData = useRecoilValue(sessionState);
 
   const { data, isLoading, isError } = useQuery("data", () =>
     axios
-      .get(`http://localhost:4000/user/${postOwner}`, {
+      .get(`http://localhost:4000/user/${username}`, {
         withCredentials: true,
       })
       .then((response) => response.data)
@@ -16,6 +21,11 @@ function UserPage() {
 
   return (
     <>
+      {sessionData.username === username ? (
+        <Link to={"/account"}>Account</Link>
+      ) : (
+        ""
+      )}
       {data ? (
         <>
           <div>{data.name}</div>
