fdff70efff234aa1cd73a35a876a1a69f288983c
Min <esspacetoM@gmail.com>
2023-08-13 22:41:52 +0900

    feat: 유저 프로필 변경 기능 추가 및 세션 쿠키 변경

--- client/src/components/Account.tsx
+++ client/src/components/Account.tsx
@@ -1,11 +1,78 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { sessionState } from "../atoms";
+import { useCookies } from "react-cookie";
 
 function Account() {
+  const [, setCookie] = useCookies(["user"]);
+  const [sessionData, setSessionData] = useRecoilState(sessionState);
+  const [formData, setFormData] = useState({
+    name: sessionData.name,
+    email: sessionData.email,
+    username: sessionData.username,
+  });
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.put(
+        `http://localhost:4000/account`,
+        formData,
+        { withCredentials: true }
+      );
+
+      setCookie("user", response.data);
+      setSessionData(response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
     <>
-      <Link to={"/account"}>프로필 수정</Link>
-      <Link to={"/account/password"}>비밀번호 변경</Link>
-      <Link to={"/account/close"}>회원탈퇴</Link>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={formData.name}
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="email"
+          value={formData.email}
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={formData.username}
+          required
+          onChange={handleChange}
+        />
+        <button type="submit">계정 업데이트</button>
+      </form>
+      <div>
+        <Link to={"/account"}>프로필 수정</Link>
+        <Link to={"/account/password"}>비밀번호 변경</Link>
+        <Link to={"/account/close"}>회원탈퇴</Link>
+      </div>
     </>
   );
 }
--- server/controllers/accountController.ts
+++ server/controllers/accountController.ts
@@ -1,6 +1,31 @@
 import { Request, Response } from "express";
+import User from "../models/User";
+import { CustomSession } from "../types/session";
 
-export const editProfile = (req: Request, res: Response) => {};
+export const editProfile = async (req: Request, res: Response) => {
+  const { name: newName, email: newEmail, username: newUsername } = req.body;
+  const session = req.session as CustomSession;
+  const username = session.user?.username;
+  const user = await User.findOneAndUpdate(
+    { username },
+    { name: newName, email: newEmail, username: newUsername }
+  );
+
+  if (session.user) {
+    const newUserSessionData = {
+      email: newEmail,
+      name: newName,
+      username: newUsername,
+      socialOnly: session.user?.socialOnly,
+      profileImage: session.user?.profileImage,
+      _id: session.user._id,
+    };
+
+    session.user = newUserSessionData;
+  }
+
+  return res.status(200).json(session.user);
+};
 
 export const closeAccount = (req: Request, res: Response) => {};
 
