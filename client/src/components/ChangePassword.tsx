3b8acfcaeab269e3e5010f951326436b8ab40abf
Min <esspacetoM@gmail.com>
2023-08-13 23:50:51 +0900

    feat: 비밀번호 변경을 위한 ChangePassword 컴포넌트 생성

--- client/src/components/ChangePassword.tsx
+++ client/src/components/ChangePassword.tsx
@@ -1,5 +1,78 @@
+import axios from "axios";
+import { useState } from "react";
+import CryptoJS from "crypto-js";
+import { useNavigate } from "react-router-dom";
+
 function ChangePassword() {
-  return null;
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    currentPassword: "",
+    newPassword: "",
+    newConfirmPassword: "",
+  });
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const hashedFormData = {
+      currentPassword: CryptoJS.SHA256(formData.currentPassword).toString(),
+      newPassword: CryptoJS.SHA256(formData.newPassword).toString(),
+      newConfirmPassword: CryptoJS.SHA256(
+        formData.newConfirmPassword
+      ).toString(),
+    };
+
+    try {
+      const response = await axios.patch(
+        `http://localhost:4000/account/password`,
+        hashedFormData,
+        { withCredentials: true }
+      );
+
+      navigate("/");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="currentPassword"
+        type="password"
+        placeholder="Current Password"
+        value={formData.currentPassword}
+        onChange={handleChange}
+        required
+      />
+      <input
+        name="newPassword"
+        type="password"
+        placeholder="New Password"
+        value={formData.newPassword}
+        onChange={handleChange}
+        required
+      />
+      <input
+        name="newConfirmPassword"
+        type="password"
+        placeholder="New Confirm Password"
+        value={formData.newConfirmPassword}
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default ChangePassword;
