318f8edbfc600c3557908e9deec6c68679b7c15f
Min <esspacetoM@gmail.com>
2023-08-14 20:41:04 +0900

    edit: 서버로부터 post data를 전달받았을 때에만 post 표시

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -8,15 +8,12 @@ import { useRecoilValue } from "recoil";
 import { loggedInState, sessionState } from "../atoms";
 
 function Posts() {
-  const { data, isLoading, isError } = useQuery("getData", () =>
+  const { data } = useQuery("getData", () =>
     axios.get("http://localhost:4000/").then((response) => response.data)
   );
 
   const loggedIn = useRecoilValue(loggedInState);
   const sessionData = useRecoilValue(sessionState);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error occurred.</div>;
 
   return (
     <div>
@@ -38,29 +35,33 @@ function Posts() {
         </>
       )}
 
-      {data.map((post: any) => (
-        <div
-          key={post._id}
-          style={{ border: "1px solid black", width: "250px" }}>
-          <h2>{post.title}</h2>
-          <h4>: {post.description}</h4>
-          <h5>{post.hashtags}</h5>
-          <h5>{post.createdAt}</h5>
-          <Link to={`/user/${post.owner.username}`} state={post.owner.username}>
-            {post.owner.name}
-          </Link>
-          <br />
-          <span>{post.views}</span>
-          {sessionData._id === post.owner._id ? (
-            <>
-              <EditPost postId={post._id} />
-              <DeletePost postId={post._id} />
-            </>
-          ) : (
-            ""
-          )}
-        </div>
-      ))}
+      {data
+        ? data.map((post: any) => (
+            <div
+              key={post._id}
+              style={{ border: "1px solid black", width: "250px" }}>
+              <h2>{post.title}</h2>
+              <h4>: {post.description}</h4>
+              <h5>{post.hashtags}</h5>
+              <h5>{post.createdAt}</h5>
+              <Link
+                to={`/user/${post.owner.username}`}
+                state={post.owner.username}>
+                {post.owner.name}
+              </Link>
+              <br />
+              <span>{post.views}</span>
+              {sessionData._id === post.owner._id ? (
+                <>
+                  <EditPost postId={post._id} />
+                  <DeletePost postId={post._id} />
+                </>
+              ) : (
+                ""
+              )}
+            </div>
+          ))
+        : ""}
     </div>
   );
 }
