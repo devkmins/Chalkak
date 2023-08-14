9e999c54bf4ab890241c113e429d78dbfdae34c9
Min <esspacetoM@gmail.com>
2023-08-14 20:44:04 +0900

    feat: 회원탈퇴 기능 추가

--- client/src/components/CloseAccount.tsx
+++ client/src/components/CloseAccount.tsx
@@ -1,5 +1,73 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CryptoJS from "crypto-js";
+import { useCookies } from "react-cookie";
+import { useSetRecoilState } from "recoil";
+import { loggedInState, sessionState } from "../atoms";
+
 function CloseAccount() {
-  return null;
+  const navigate = useNavigate();
+
+  const setLoggedIn = useSetRecoilState(loggedInState);
+  const setSessionData = useSetRecoilState(sessionState);
+  const [, , removeCookie] = useCookies(["connect.sid", "loggedIn", "user"]);
+
+  const [formData, setFormData] = useState({
+    password: "",
+  });
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const hashedFormData = {
+      password: CryptoJS.SHA256(formData.password).toString(),
+    };
+
+    try {
+      const response = await axios.delete(
+        `http://localhost:4000/account/close`,
+        { withCredentials: true, data: hashedFormData }
+      );
+
+      setLoggedIn(false);
+      setSessionData({
+        email: "",
+        username: "",
+        name: "",
+        socialOnly: false,
+        _id: "",
+      });
+      removeCookie("connect.sid");
+      removeCookie("loggedIn");
+      removeCookie("user");
+
+      navigate("/");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+        required
+      />
+    </form>
+  );
 }
 
 export default CloseAccount;
--- server/controllers/accountController.ts
+++ server/controllers/accountController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import User from "../models/User";
 import { CustomSession } from "../types/session";
 import bcrypt from "bcrypt";
+import Post from "../models/Post";
 
 export const editProfile = async (req: Request, res: Response) => {
   const { name: newName, email: newEmail, username: newUsername } = req.body;
@@ -28,7 +29,38 @@ export const editProfile = async (req: Request, res: Response) => {
   return res.status(200).json(session.user);
 };
 
-export const closeAccount = (req: Request, res: Response) => {};
+export const closeAccount = async (req: Request, res: Response) => {
+  if (res.headersSent) {
+    return;
+  }
+
+  const { password } = req.body;
+  const session = req.session as CustomSession;
+  const username = session.user?.username;
+  const user = await User.findOne({ username });
+
+  if (user) {
+    const comparePassword = await bcrypt.compare(password, user?.password);
+
+    if (comparePassword) {
+      const posts = user.posts;
+
+      posts.forEach(async (post) => await Post.findByIdAndDelete(post));
+
+      session.destroy((error) => {
+        if (error) {
+          return res.send("error");
+        } else {
+          return res.send("Logout");
+        }
+      });
+
+      await User.findOneAndDelete({ username });
+    }
+  }
+
+  return res.status(200);
+};
 
 export const changePassword = async (req: Request, res: Response) => {
   const { currentPassword, newPassword, newConfirmPassword } = req.body;
