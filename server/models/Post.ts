d5de30f29b255af2a8a17009d92ec3677bb19d2a
Min <esspacetoM@gmail.com>
2023-08-16 23:56:43 +0900

    edit: Post 스키마의 fileUrl 필드 내의 값의 타입을 Object로 변경

--- server/models/Post.ts
+++ server/models/Post.ts
@@ -3,7 +3,7 @@ import mongoose, { Types } from "mongoose";
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 75 },
   description: { type: String, required: true, trim: true, minLength: 1 },
-  fileUrl: [{ type: String, required: true }],
+  fileUrl: [{ type: Object, required: true }],
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
