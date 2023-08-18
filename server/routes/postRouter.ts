cf7ca1163978da2bed8a693e58a931636fdbb8aa
Min <esspacetoM@gmail.com>
2023-08-18 23:54:30 +0900

    feat: /upload/images 경로에 라우트 추가

--- server/routes/postRouter.ts
+++ server/routes/postRouter.ts
@@ -4,13 +4,19 @@ import {
   postUpload,
   postEdit,
   postDelete,
+  imagesUpload,
 } from "../controllers/postController";
 import { imageUpload, protectorMiddleware } from "../middlewares";
 
 const postRouter = express.Router();
 
 postRouter.get("/:pid([0-9a-f]{24})", watch);
-postRouter.post("/upload", imageUpload.array("images", 10), postUpload);
+postRouter.post(
+  "/upload/images",
+  imageUpload.array("images[]", 10),
+  imagesUpload
+);
+postRouter.post("/upload", postUpload);
 postRouter.put("/:pid([0-9a-f]{24})/edit", postEdit);
 postRouter.delete("/:pid([0-9a-f]{24})/delete", postDelete);
 
