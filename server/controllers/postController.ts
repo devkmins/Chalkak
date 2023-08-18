716b4dcf3eea7738bedc8f7864b69dcfb36b0ac3
Min <esspacetoM@gmail.com>
2023-08-18 23:56:26 +0900

    feat: postUpload 수정 및 imagesUpload 핸들러를 추가함으로써 이미지 파일을 DB에 저장

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Post from "../models/Post";
 import { CustomSession } from "../types/session";
 import User from "../models/User";
+import multer from "multer";
+import path from "path";
 
 export const watch = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -17,14 +19,38 @@ export const watch = async (req: Request, res: Response) => {
 export const postUpload = async (req: Request, res: Response) => {
   const session = req.session as CustomSession;
   const userId = session.user?._id;
-  const { title, description, hashtags } = req.body;
+  const { title, description, hashtags } = req.body.formData;
+  const postId = req.body.postId;
+  const post = await Post.findById(postId);
+
+  if (post?.owner.toString() !== userId) {
+    return res.status(403).send({ error: "권한이 없습니다." });
+  }
+
+  await Post.findByIdAndUpdate(postId, {
+    title,
+    description,
+    hashtags,
+  });
+
+  return res.status(200).json();
+};
+
+export const imagesUpload = async (req: Request, res: Response) => {
+  const session = req.session as CustomSession;
+  const userId = session.user?._id;
+  let paths;
+
+  if (Array.isArray(req.files)) {
+    paths = req.files?.filter((data) => data.path);
+  }
 
   try {
     const newPost = await Post.create({
-      title,
-      description,
-      fileUrl: "example",
-      hashtags,
+      title: "Default",
+      description: "Default",
+      fileUrl: paths,
+      hashtags: "",
       owner: userId,
     });
 
@@ -36,12 +62,12 @@ export const postUpload = async (req: Request, res: Response) => {
       user?.posts.push(newPost._id);
       await user?.save();
     }
+
+    return res.status(200).json(newPost._id);
   } catch (error) {
     console.log(error);
     return res.status(400).send("404");
   }
-
-  return res.status(200).json();
 };
 
 export const postEdit = async (req: Request, res: Response) => {
