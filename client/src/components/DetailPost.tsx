292740dc7ff121a051186a5704ebfeeae1af8a7e
Min <esspacetoM@gmail.com>
2023-08-21 23:59:24 +0900

    feat: 상세 페이지 추가

--- client/src/components/DetailPost.tsx
+++ client/src/components/DetailPost.tsx
@@ -1,5 +1,54 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { Link, useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { sessionState } from "../atoms";
+import EditPost from "./EditPost";
+import DeletePost from "./DeletePost";
+
 function DetailPost() {
-  return null;
+  const params = useParams();
+  const postId = params.id;
+
+  const sessionData = useRecoilValue(sessionState);
+
+  const { data } = useQuery("getData", () =>
+    axios
+      .get(`http://localhost:4000/post/${postId}`)
+      .then((response) => response.data)
+  );
+
+  return (
+    <>
+      {data && (
+        <div key={data._id}>
+          {data.fileUrl?.map((img: any) => (
+            <img
+              key={img.path}
+              alt=""
+              src={`http://localhost:4000/${img.path}`}
+            />
+          ))}
+          <div>
+            <h3>{data.title}</h3>
+            <h3>{data.description}</h3>
+            <h3>{data.hashtags}</h3>
+            <h4>{data.createdAt}</h4>
+            <h4>{data.views}</h4>
+          </div>
+          <Link to="/">HOme</Link>
+          {sessionData._id === data.owner ? (
+            <>
+              <EditPost postId={data._id} />
+              <DeletePost postId={data._id} />
+            </>
+          ) : (
+            ""
+          )}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default DetailPost;
