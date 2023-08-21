6090fcfd90de702947324e69a1c866db693eafdb
Min <esspacetoM@gmail.com>
2023-08-21 23:59:33 +0900

    feat: 게시글 제거 시에 /로 이동

--- client/src/components/DeletePost.tsx
+++ client/src/components/DeletePost.tsx
@@ -1,12 +1,17 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function DeletePost({ postId }: any) {
+  const navigate = useNavigate();
+
   const onClick = async () => {
     try {
       const response = await axios.delete(
         `http://localhost:4000/post/${postId}/delete`,
         { withCredentials: true }
       );
+
+      navigate("/");
     } catch (error) {
       console.error("Error:", error);
     }
