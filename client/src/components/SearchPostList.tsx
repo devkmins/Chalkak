f0ecd945190c546697e186ccab385d2b8a6eac63
Min <esspacetoM@gmail.com>
2023-08-24 23:58:32 +0900

    feat: 검색 시에 연관된 posts를 불러오는 SearchPostList 컴포넌트 생성

---
+++ client/src/components/SearchPostList.tsx
@@ -0,0 +1,43 @@
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  display: flex;
+  max-width: 250px;
+`;
+
+function SearchPostList() {
+  const location = useLocation();
+  const posts = location.state;
+
+  console.log(posts);
+
+  return (
+    <>
+      {posts &&
+        posts.map((post: any) => (
+          <div
+            key={post._id}
+            style={{ border: "1px solid black", maxWidth: "250px" }}>
+            <StyledLink to={`/post/${post.title}`} state={post._id}>
+              {post.fileUrl.map((img: any) => (
+                <img
+                  key={img.path}
+                  alt=""
+                  src={`http://localhost:4000/${img.path}`}
+                />
+              ))}
+            </StyledLink>
+            <br />
+            <Link
+              to={`/user/${post.owner.username}`}
+              state={post.owner.username}>
+              {post.owner.name}
+            </Link>
+          </div>
+        ))}
+    </>
+  );
+}
+
+export default SearchPostList;
