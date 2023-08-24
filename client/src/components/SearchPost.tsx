8615949bdaf1cd328461195349eef6a7cb861f1f
Min <esspacetoM@gmail.com>
2023-08-24 23:57:17 +0900

    feat: 검색 후 서버에 params로 keyword를 전달하는 SearchPost 컴포넌트 생성

---
+++ client/src/components/SearchPost.tsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchPost() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    keyword: "",
+  });
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const response = await axios.get(
+      `http://localhost:4000/search/${formData.keyword}`,
+      { withCredentials: true }
+    );
+
+    navigate(`/search/${formData.keyword}`, { state: response.data });
+  };
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="search"
+        onChange={handleChange}
+        value={formData.keyword}
+        placeholder="검색하기"
+        type="text"
+        alt=""
+      />
+    </form>
+  );
+}
+
+export default SearchPost;
