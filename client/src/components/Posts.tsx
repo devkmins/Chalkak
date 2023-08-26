a7ebd86791f0ff67647b548da19137fa83e75d62
Min <esspacetoM@gmail.com>
2023-08-26 23:57:48 +0900

    remove: loggedIn 판별 코드 제거

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -1,10 +1,7 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
-import { useRecoilValue } from "recoil";
-import { loggedInState, sessionState } from "../atoms";
 import styled from "styled-components";
-import SearchPost from "./SearchPost";
 
 const StyledLink = styled(Link)`
   display: flex;
@@ -16,31 +13,8 @@ function Posts() {
     axios.get("http://localhost:4000/").then((response) => response.data)
   );
 
-  const loggedIn = useRecoilValue(loggedInState);
-  const sessionData = useRecoilValue(sessionState);
-
   return (
     <div>
-      {loggedIn ? (
-        <>
-          {sessionData ? <h2>안녕하세요 {sessionData?.name}님.</h2> : ""}
-          <Link to={"/user/logout"}>Logout</Link>
-          <Link
-            to={`/user/${sessionData.username}`}
-            state={sessionData.username}>
-            My Profile
-          </Link>
-          <Link to={"/post/upload"}>Upload</Link>
-        </>
-      ) : (
-        <>
-          <Link to={"/join"}>Join</Link>
-          <Link to={"/login"}>Login</Link>
-        </>
-      )}
-
-      <SearchPost />
-
       {Array.isArray(data)
         ? data.map((post: any) => (
             <div
