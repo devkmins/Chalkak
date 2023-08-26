53a9a308933ae88be31d356fa1d94ea5893f6e97
Min <esspacetoM@gmail.com>
2023-08-26 23:56:30 +0900

    feat: Main 컴포넌트 생성 및 Header, Posts 컴포넌트 렌더링

---
+++ client/src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import Posts from "../../components/Posts";
+import Header from "../Header";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <Posts />
+    </>
+  );
+}
+
+export default Main;
