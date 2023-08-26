8255abc1544c684f04a86e054f795cfa65284f5e
Min <esspacetoM@gmail.com>
2023-08-26 23:39:12 +0900

    feat: GlobalStyle 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,67 @@ import axios from "axios";
 import DetailPost from "./components/DetailPost";
 import SearchPostList from "./components/SearchPostList";
 import Main from "./pages/Main";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Source+Sans+Pro:wght@300;400&display=swap');
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li, 
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video { 
+	margin: 0;
+ 	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+* {
+	box-sizing: border-box;
+}
+body {
+  font-family: 'NanumGothicLight';
+  font-family: 'NanumGothic';
+  font-family: 'NanumGothicBold';
+  font-family: 'NanumGothicExtraBold';
+}
+a {
+	text-decoration: none;
+	color: inherit;
+}
+`;
 
 function App() {
   const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
@@ -33,6 +94,7 @@ function App() {
 
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/user/:id" element={<UserPage />} />
