c8a0587a43c8bd0d99015c42ac0f77969be9af48
Min <esspacetoM@gmail.com>
2023-08-27 15:59:27 +0900

    design: Join 컴포넌트 스타일 및 login 로직 제거

--- client/src/pages/User/Join.tsx
+++ client/src/pages/User/Join.tsx
@@ -4,6 +4,118 @@ import { useNavigate } from "react-router-dom";
 import CryptoJS from "crypto-js";
 import { useSetRecoilState } from "recoil";
 import { loggedInState, sessionState } from "../../atoms";
+import styled from "styled-components";
+import joinImg from "../../assets/Join/join.jpeg";
+import { RiCameraLensFill } from "react-icons/ri";
+
+const Box = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const JoinImgContainer = styled.div``;
+
+const JoinImg = styled.div`
+  position: fixed;
+  background-image: url(${joinImg});
+  width: 50%;
+  height: 100%;
+  background-position: center center;
+  background-size: cover;
+  max-height: 100vh;
+  overflow: hidden;
+`;
+
+const JoinBox = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-rows: 25% 75%;
+  height: 150vh;
+  overflow: scroll;
+`;
+
+const JoinLogo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 12.5px;
+
+  span {
+    margin-top: 20px;
+    font-size: 24px;
+    font-weight: 500;
+  }
+
+  hr {
+    margin-top: 30px;
+    width: 75%;
+  }
+`;
+
+const JoinLogoIcon = styled(RiCameraLensFill)`
+  width: 80px;
+  height: 80px;
+`;
+
+const JoinForm = styled.form`
+  display: grid;
+  grid-template-rows: repeat(6, 17.5%);
+  grid-template-columns: 75%;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+`;
+
+const JoinInputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    margin-bottom: 5px;
+    font-weight: 300;
+    font-weight: 500;
+  }
+
+  input {
+    border-radius: 5px;
+    border: 1px solid gray;
+    height: 36%;
+    padding-left: 10px;
+    font-size: 15px;
+
+    &:focus {
+      border: 1.75px solid #636e72;
+    }
+  }
+`;
+
+const JoinBtn = styled.button`
+  border-radius: 5px;
+  border: 1px solid gray;
+  background-color: black;
+  color: white;
+  height: 40%;
+  font-weight: 500;
+  font-size: 15px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ErrorMessage = styled.span`
+  margin-top: 7.5px;
+  font-size: 13px;
+  color: #ff6b6b;
+`;
+
+interface Error {
+  usernameError: string;
+  emailError: string;
+  passwordError: string;
+}
 
 function Join() {
   const navigate = useNavigate();
@@ -16,7 +128,7 @@ function Join() {
     confirmPassword: "",
   });
 
-  const [error, setError] = useState("");
+  const [error, setError] = useState<Error>();
 
   const setLoggedIn = useSetRecoilState(loggedInState);
   const setSessionData = useSetRecoilState(sessionState);
@@ -41,18 +153,8 @@ function Join() {
       .post("http://localhost:4000/join", hashedJoinFormData, {
         withCredentials: true,
       })
-      .catch((error) => setError(error.response.data.error));
-
-    const login = await axios
-      .post("http://localhost:4000/login", hashedLoginFormData, {
-        withCredentials: true,
-      })
-      .then((response) => {
-        setLoggedIn(true);
-        setSessionData(response.data.user);
-        navigate("/");
-      })
-      .catch((error) => setError(error.response.data.error));
+      .then(() => navigate("/login"))
+      .catch((error) => setError(error.response.data));
   };
 
   const handleChange = (event: any) => {
@@ -62,55 +164,89 @@ function Join() {
       ...prevFormData,
       [name]: value,
     }));
+
+    setError({
+      usernameError: "",
+      emailError: "",
+      passwordError: "",
+    });
   };
 
   return (
-    <>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          name="name"
-          type="text"
-          placeholder="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <input
-          name="email"
-          type="email"
-          placeholder="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          name="username"
-          type="text"
-          placeholder="username"
-          value={formData.username}
-          onChange={handleChange}
-          required
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <input
-          name="confirmPassword"
-          type="password"
-          placeholder="confirm your password"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+    <Box>
+      <JoinImgContainer>
+        <JoinImg />
+      </JoinImgContainer>
+      <JoinBox>
+        <JoinLogo>
+          <JoinLogoIcon />
+          <span>회원가입</span>
+          <hr />
+        </JoinLogo>
+        <JoinForm onSubmit={handleSubmit}>
+          <JoinInputBox>
+            <span>이름</span>
+            <input
+              name="name"
+              type="text"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </JoinInputBox>
+          <JoinInputBox>
+            <span>이메일</span>
+            <input
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            {error && error.emailError && (
+              <ErrorMessage>{error.emailError}</ErrorMessage>
+            )}
+          </JoinInputBox>
+          <JoinInputBox>
+            <span>아이디</span>
+            <input
+              name="username"
+              type="text"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+            {error && error.usernameError && (
+              <ErrorMessage>{error.usernameError}</ErrorMessage>
+            )}
+          </JoinInputBox>
+          <JoinInputBox>
+            <span>비밀번호</span>
+            <input
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </JoinInputBox>
+          <JoinInputBox>
+            <span>비밀번호 확인</span>
+            <input
+              name="confirmPassword"
+              type="password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+            {error && error.passwordError && (
+              <ErrorMessage>{error.passwordError}</ErrorMessage>
+            )}
+          </JoinInputBox>
+          <JoinBtn type="submit">Submit</JoinBtn>
+        </JoinForm>
+      </JoinBox>
+    </Box>
   );
 }
 
