c89410d7a1d27850cb3406d41a89945c4ef10285
Min <esspacetoM@gmail.com>
2023-08-27 20:47:59 +0900

    remove: 주석 제거

--- client/src/pages/User/Login.tsx
+++ client/src/pages/User/Login.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CryptoJS from "crypto-js";
 import { useSetRecoilState } from "recoil";
@@ -203,7 +203,5 @@ function Login() {
     </Box>
   );
 }
-
-// {error?.passwordError && <span>{error}</span>}
 
 export default Login;
