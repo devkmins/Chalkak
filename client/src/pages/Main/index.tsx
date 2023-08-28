7fd12c2977c0577f41ab818ffe8041fc165a7a5a
Min <esspacetoM@gmail.com>
2023-08-28 22:01:39 +0900

    feat: IntroContent 추가

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,10 +1,12 @@
 import Posts from "../../components/Posts";
 import Header from "../Header";
+import IntroContent from "../../components/IntroContent";
 
 function Main() {
   return (
     <>
       <Header />
+      <IntroContent />
       <Posts />
     </>
   );
