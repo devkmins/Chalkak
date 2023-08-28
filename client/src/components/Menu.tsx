29c324273c659a55cf1edca9aa3c0b899d046b58
Min <esspacetoM@gmail.com>
2023-08-28 23:57:41 +0900

    design: 프로필 보기 Link와 계정 설정 Link를 렌더링하는 Menu 컴포넌트 스타일

---
+++ client/src/components/Menu.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { sessionState } from "../atoms";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: absolute;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  align-items: center;
+  min-width: max-content;
+  min-height: 80px;
+  background-color: white;
+  border: 1px solid lightgray;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  border-top-left-radius: 10px;
+  margin-top: 2.5px;
+  right: 35px;
+
+  a {
+    font-size: 15px;
+  }
+`;
+
+function Menu() {
+  const sessionData = useRecoilValue(sessionState);
+
+  return (
+    <Container>
+      <Link to={`/user/${sessionData.username}`} state={sessionData.username}>
+        프로필 보기
+      </Link>
+      <Link to={"/account"}>계정 설정</Link>
+    </Container>
+  );
+}
+
+export default Menu;
