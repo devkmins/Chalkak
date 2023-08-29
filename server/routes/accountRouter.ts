432dbb6d27969379ad1e3660515fdb4bbaa4acf5
Min <esspacetoM@gmail.com>
2023-08-29 22:42:36 +0900

    feat: /account/profileImg 경로에 라우트 추가 및 avatarImgUpload 미들웨어 적용

--- server/routes/accountRouter.ts
+++ server/routes/accountRouter.ts
@@ -3,11 +3,18 @@ import {
   changePassword,
   closeAccount,
   editProfile,
+  editProfileImg,
 } from "../controllers/accountController";
+import { avatarImgUpload } from "../middlewares";
 
 const accountRouter = express.Router();
 
 accountRouter.put("/", editProfile);
+accountRouter.post(
+  "/profileImg",
+  avatarImgUpload.single("profileImg"),
+  editProfileImg
+);
 accountRouter.delete("/close", closeAccount);
 accountRouter.patch("/password", changePassword);
 
