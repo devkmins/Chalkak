68bc85d97c11d5760dcf5ff3b889183756d1114c
Min <esspacetoM@gmail.com>
2023-08-30 18:02:58 +0900

    design: AccountMenu 컴포넌트 생성 및 스타일

---
+++ client/src/components/AccountMenu.tsx
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+interface AccountMenuProp {
+  pathname: string;
+}
+
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Text = styled.span`
+  margin-left: 22.5px;
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const LinkUl = styled.ul`
+  margin-top: 25px;
+`;
+
+const LinkLi = styled.li`
+  padding: 17.5px 22.5px;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 16px;
+  font-weight: 300;
+
+  &:hover {
+    color: #060606;
+    font-weight: 350;
+  }
+`;
+
+const ConnectedtLink = styled(Link)`
+  font-size: 16px;
+  font-weight: 350;
+  color: #060606;
+`;
+
+function AccountMenu({ pathname }: AccountMenuProp) {
+  return (
+    <LinkContainer>
+      <Text>계정 설정</Text>
+      <LinkUl>
+        <LinkLi>
+          {pathname === "/account" ? (
+            <ConnectedtLink to={"/account"}>프로필 편집</ConnectedtLink>
+          ) : (
+            <StyledLink to={"/account"}>프로필 편집</StyledLink>
+          )}
+        </LinkLi>
+        <LinkLi>
+          {pathname === "/account/password" ? (
+            <ConnectedtLink to={"/account/password"}>
+              비밀번호 변경
+            </ConnectedtLink>
+          ) : (
+            <StyledLink to={"/account/password"}>비밀번호 변경</StyledLink>
+          )}
+        </LinkLi>
+        <LinkLi>
+          {pathname === "/account/close" ? (
+            <ConnectedtLink to={"/account/close"}>회원탈퇴</ConnectedtLink>
+          ) : (
+            <StyledLink to={"/account/close"}>회원탈퇴</StyledLink>
+          )}
+        </LinkLi>
+      </LinkUl>
+    </LinkContainer>
+  );
+}
+
+export default AccountMenu;
