7c3cdd82a66822d6afb550cb930e7f4d3c7cce13
Min <esspacetoM@gmail.com>
2023-09-02 23:03:32 +0900

    design: Posts 컴포넌트 스타일

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -1,11 +1,83 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+  width: 100%;
+`;
+
+const ColumnsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 31.5%);
+  grid-auto-rows: auto;
+  grid-gap: 25px;
+  justify-content: center;
+  padding: 0px 15px;
+  padding-top: 50px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: auto;
+  }
+`;
+
+const ImagesContainer = styled.div``;
+
 const StyledLink = styled(Link)`
   display: flex;
-  max-width: 250px;
+  width: 100%;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  max-height: max-content;
+`;
+
+const ProfileContainer = styled.div`
+  display: none;
+  position: absolute;
+  bottom: 0;
+  width: max-content;
+  height: max-content;
+  margin-bottom: 17.5px;
+`;
+
+const ProfileBox = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0px 20px;
+`;
+
+const ProfileLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  color: #e0dfdf;
+  font-size: 16px;
+
+  &:hover {
+    color: white;
+    transition: color 0.25s;
+  }
+`;
+
+const ProfileImg = styled.img`
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const ImagesBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin-bottom: 25px;
+
+  &:hover {
+    ${ProfileContainer} {
+      display: flex;
+    }
+  }
 `;
 
 function Posts() {
@@ -13,32 +85,118 @@ function Posts() {
     axios.get("http://localhost:4000/").then((response) => response.data)
   );
 
+  const [firstCol, setFirstCol] = useState<any[]>([]);
+  const [secondCol, setSecondCol] = useState<any[]>([]);
+  const [thirdCol, setThirdCol] = useState<any[]>([]);
+
+  useEffect(() => {
+    const firstColImages: string[] = [];
+    const secondColImages: string[] = [];
+    const thirdColImages: string[] = [];
+
+    if (data && Array.isArray(data)) {
+      data?.forEach((post: any, index: any) => {
+        if (index % 3 === 0) {
+          firstColImages.push(post);
+        } else if (index % 3 === 1) {
+          secondColImages.push(post);
+        } else if (index % 3 === 2) {
+          thirdColImages.push(post);
+        }
+      });
+    }
+
+    setFirstCol(firstColImages);
+    setSecondCol(secondColImages);
+    setThirdCol(thirdColImages);
+  }, [data]);
+
   return (
-    <div>
-      {Array.isArray(data)
-        ? data.map((post: any) => (
-            <div
-              key={post._id}
-              style={{ border: "1px solid black", maxWidth: "250px" }}>
-              <StyledLink to={`/post/${post.title}`} state={post._id}>
-                {post.fileUrl.map((img: any) => (
-                  <img
-                    key={img.path}
+    <Container>
+      <ColumnsContainer>
+        <ImagesContainer>
+          {firstCol &&
+            firstCol.map((post) => (
+              <ImagesBox key={post?._id}>
+                <StyledLink to={`/post/${post?.title}`} state={post?._id}>
+                  <Image
+                    src={`http://localhost:4000/${post.fileUrl[0].path}`}
                     alt=""
-                    src={`http://localhost:4000/${img.path}`}
                   />
-                ))}
-              </StyledLink>
-              <br />
-              <Link
-                to={`/user/${post.owner.username}`}
-                state={post.owner.username}>
-                {post.owner.name}
-              </Link>
-            </div>
-          ))
-        : ""}
-    </div>
+                </StyledLink>
+                <ProfileContainer>
+                  <ProfileBox>
+                    <ProfileLink
+                      to={`/user/${post.owner.username}`}
+                      state={post.owner.username}>
+                      <ProfileImg
+                        src={`http://localhost:4000/${post.owner.profileImage}`}
+                        alt=""
+                        style={{ width: "30px", height: "30px" }}
+                      />
+                      {post.owner.name}
+                    </ProfileLink>
+                  </ProfileBox>
+                </ProfileContainer>
+              </ImagesBox>
+            ))}
+        </ImagesContainer>
+        <ImagesContainer>
+          {secondCol &&
+            secondCol.map((post) => (
+              <ImagesBox key={post?._id}>
+                <StyledLink to={`/post/${post?.title}`} state={post?._id}>
+                  <Image
+                    src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                    alt=""
+                  />
+                </StyledLink>
+                <ProfileContainer>
+                  <ProfileBox>
+                    <ProfileImg
+                      src={`http://localhost:4000/${post.owner.profileImage}`}
+                      alt=""
+                      style={{ width: "30px", height: "30px" }}
+                    />
+                    <ProfileLink
+                      to={`/user/${post.owner.username}`}
+                      state={post.owner.username}>
+                      {post.owner.name}
+                    </ProfileLink>
+                  </ProfileBox>
+                </ProfileContainer>
+              </ImagesBox>
+            ))}
+        </ImagesContainer>
+        <ImagesContainer>
+          {thirdCol &&
+            thirdCol.map((post) => (
+              <ImagesBox key={post?._id}>
+                <StyledLink to={`/post/${post?.title}`} state={post?._id}>
+                  <Image
+                    src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                    alt=""
+                  />
+                </StyledLink>
+                <ProfileContainer>
+                  <ProfileBox>
+                    <ProfileImg
+                      src={`http://localhost:4000/${post.owner.profileImage}`}
+                      alt=""
+                      style={{ width: "30px", height: "30px" }}
+                    />
+                    <ProfileLink
+                      to={`/user/${post.owner.username}`}
+                      state={post.owner.username}>
+                      {post.owner.name}
+                    </ProfileLink>
+                  </ProfileBox>
+                </ProfileContainer>
+              </ImagesBox>
+            ))}
+        </ImagesContainer>
+      </ColumnsContainer>
+    </Container>
   );
 }
 
