ac5641d34c0dda19ed3fd1890bd28ff1d9d31436
Min <esspacetoM@gmail.com>
2023-09-02 22:02:57 +0900

    remove: description 필드의 required, minLength 옵션 제거

--- server/models/Post.ts
+++ server/models/Post.ts
@@ -2,7 +2,7 @@ import mongoose, { Types } from "mongoose";
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 75 },
-  description: { type: String, required: true, trim: true, minLength: 1 },
+  description: { type: String, trim: true, maxLength: 150 },
   fileUrl: [{ type: Object, required: true }],
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
