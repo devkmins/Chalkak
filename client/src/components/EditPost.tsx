cb8f428c78ef62f56426225a57c5402d8a39e37f
Min <esspacetoM@gmail.com>
2023-09-13 22:13:24 +0900

    design: EditPost 컴포넌트 스타일

--- client/src/components/EditPost.tsx
+++ client/src/components/EditPost.tsx
@@ -1,11 +1,102 @@
 import axios from "axios";
 import { useState } from "react";
+import { styled } from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 75%;
+  height: 100%;
+`;
+
+const TitleInput = styled.input`
+  margin-top: 5px;
+  margin-bottom: 25px;
+  height: 40px;
+  font-family: "NanumGothic";
+`;
+
+const DescriptionTextArea = styled.textarea`
+  margin-top: 5px;
+  margin-bottom: 25px;
+  height: 120px;
+  overflow-wrap: break-word;
+  resize: none;
+  font-family: "NanumGothic";
+`;
+
+const HashtagsInput = styled.input`
+  margin-top: 5px;
+  margin-bottom: 25px;
+  height: 40px;
+  font-family: "NanumGothic";
+`;
+
+const HashtagsBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  white-space: nowrap;
+  margin-bottom: 15px;
+`;
+
+const Hashtag = styled.div`
+  padding: 10px 25px;
+  border-radius: 7.5px;
+  border: 1px solid #767676;
+  width: min-content;
+  margin-right: 15px;
+  margin-bottom: 10px;
+  color: #8c8c8c;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 300;
+`;
+
+const RemoveButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 1px;
+  right: 15px;
+  color: white;
+  border: none;
+  border-radius: 50%;
+  width: 15px;
+  height: 15px;
+  line-height: 15px;
+  font-size: 10px;
+  text-align: center;
+  cursor: pointer;
+  opacity: 0;
+  transition: opacity 0.15s ease-in-out;
+`;
+
+const Hashtags = styled.div`
+  position: relative;
+
+  &:hover ${RemoveButton} {
+    opacity: 1;
+    background-color: #8c8c8c;
+  }
+`;
+
+const Btn = styled.button`
+  width: 100%;
+  height: 40px;
+  color: white;
+  background-color: black;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  font-size: 16px;
+`;
 
 function EditPost({ postId }: any) {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
-    hashtags: "",
+    hashtags: [],
   });
 
   const handleSubmit = async (event: any) => {
@@ -30,36 +121,74 @@ function EditPost({ postId }: any) {
     }));
   };
 
+  const handleHashtagsEnter = (event: any) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+
+      const { value } = event.target;
+
+      if (formData.hashtags.length < 5) {
+        setFormData((prev: any) => {
+          return { ...prev, hashtags: [...prev.hashtags, value] };
+        });
+      }
+
+      event.target.value = "";
+    }
+  };
+
+  const removeClick = (hashtag: string) => {
+    setFormData((prev: any) => {
+      const newHashtags: string[] = [...prev.hashtags];
+      const index = newHashtags.indexOf(hashtag);
+      newHashtags.splice(index, 1);
+
+      const newFormData = { ...prev, hashtags: newHashtags };
+
+      return newFormData;
+    });
+  };
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
+      <Form onSubmit={handleSubmit}>
+        <span>제목</span>
+        <TitleInput
           type="title"
           name="title"
-          placeholder="title"
           value={formData.title}
           maxLength={75}
           required
           onChange={handleChange}
         />
-        <input
-          type="text"
+        <span>설명</span>
+        <DescriptionTextArea
           name="description"
-          placeholder="description"
           value={formData.description}
           maxLength={150}
           required
           onChange={handleChange}
         />
-        <input
+        <span></span>
+        <span>해시태그</span>
+        <HashtagsInput
           type="text"
           name="hashtags"
-          placeholder="hashtags"
-          value={formData.hashtags}
-          onChange={handleChange}
+          onKeyDown={handleHashtagsEnter}
         />
-        <button type="submit">Submit</button>
-      </form>
+        <HashtagsBox>
+          {formData.hashtags &&
+            formData.hashtags.map((hashtag) => (
+              <Hashtags key={hashtag + Math.random()}>
+                <Hashtag>{hashtag}</Hashtag>
+                <RemoveButton onClick={() => removeClick(hashtag)}>
+                  X
+                </RemoveButton>
+              </Hashtags>
+            ))}
+        </HashtagsBox>
+        <Btn type="submit">업데이트</Btn>
+      </Form>
     </>
   );
 }
