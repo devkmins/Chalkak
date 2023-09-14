272a2700907df7f8e700973ded0f5562752424e6
Min <esspacetoM@gmail.com>
2023-09-14 23:56:26 +0900

    feat: TopViewsPosts 컴포넌트 추가

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,12 +1,14 @@
 import Posts from "../../components/Posts";
 import Header from "../Header";
 import IntroContent from "../../components/IntroContent";
+import TopViewsPosts from "../../components/TopViewsPosts";
 
 function Main() {
   return (
     <>
       <Header />
       <IntroContent />
+      <TopViewsPosts />
       <Posts />
     </>
   );
