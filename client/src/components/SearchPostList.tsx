c22352bef85ed82c2cd8754d30c15033ec544d70
Min <esspacetoM@gmail.com>
2023-09-14 17:45:46 +0900

    feat: SearchPostList에서 검색 결과가 없을 때 NoSearchResults 컴포넌트를 렌더링 하도록 설정

--- client/src/components/SearchPostList.tsx
+++ client/src/components/SearchPostList.tsx
@@ -4,6 +4,7 @@ import Header from "../pages/Header";
 import defaultUserProfileImg from "../assets/User/default-profile.png";
 import { useEffect, useState } from "react";
 import { RiImage2Fill } from "react-icons/ri";
+import NoSearchResults from "./NoSearchResults";
 
 const Container = styled.div``;
 
@@ -44,6 +45,7 @@ const ContentsContainer = styled.div`
   font-weight: 600;
   padding-left: 25px;
   border-bottom: 1px solid #d1d1d1;
+  margin-bottom: 30px;
 `;
 
 const PhotoBox = styled.div`
@@ -57,12 +59,20 @@ const StyledRiImage2Fill = styled(RiImage2Fill)`
   width: 25px;
   height: 25px;
   margin-right: 5px;
+  margin-bottom: 2.5px;
 `;
 
 const PostsBox = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 25px;
+`;
+
+const SearchWord = styled.span`
+  font-size: 27px;
+  font-weight: 800;
+  font-family: "NanumGothicBold";
+  margin-left: 25px;
 `;
 
 const StyledLink = styled(Link)`
@@ -127,6 +137,11 @@ function SearchPostList() {
   const location = useLocation();
   const posts = location.state;
 
+  const pathnameSplit = location.pathname.split("/");
+  const searchWord = decodeURIComponent(
+    pathnameSplit[pathnameSplit.length - 1]
+  );
+
   const [firstCol, setFirstCol] = useState<any[]>([]);
   const [secondCol, setSecondCol] = useState<any[]>([]);
   const [thirdCol, setThirdCol] = useState<any[]>([]);
@@ -154,113 +169,129 @@ function SearchPostList() {
   }, [posts]);
 
   return (
-    <Container>
-      <Header />
-      <Box>
-        <PostsContainer>
-          <ContentsContainer>
-            <PhotoBox>
-              <StyledRiImage2Fill />
-              <span>사진 {posts?.length}</span>
-            </PhotoBox>
-          </ContentsContainer>
-          <PostsBox>
-            <ColumnsContainer>
-              <ImagesContainer>
-                {firstCol &&
-                  firstCol.map((post) => (
-                    <PostBox key={post?._id}>
-                      <StyledLink to={`/post/${post?.title}`} state={post?._id}>
-                        <Image
-                          src={`http://localhost:4000/${post.fileUrl[0].path}`}
-                          alt=""
-                        />
-                      </StyledLink>
-                      <PostProfileContainer>
-                        <ProfileBox>
-                          <ProfileLink
-                            to={`/user/${post.owner.username}`}
-                            state={post.owner.username}>
-                            <PostProfileImg
-                              src={
-                                post.owner.profileImage
-                                  ? `http://localhost:4000/${post.owner.profileImage}`
-                                  : defaultUserProfileImg
-                              }
+    <>
+      {posts?.length > 0 ? (
+        <Container>
+          <Header />
+          <Box>
+            <PostsContainer>
+              <ContentsContainer>
+                <PhotoBox>
+                  <StyledRiImage2Fill />
+                  <span>사진 {posts?.length}</span>
+                </PhotoBox>
+              </ContentsContainer>
+              <SearchWord>{searchWord}</SearchWord>
+              <PostsBox>
+                <ColumnsContainer>
+                  <ImagesContainer>
+                    {firstCol &&
+                      firstCol.map((post) => (
+                        <PostBox key={post?._id}>
+                          <StyledLink
+                            to={`/post/${post?.title}`}
+                            state={post?._id}>
+                            <Image
+                              src={`http://localhost:4000/${post.fileUrl[0].path}`}
                               alt=""
                             />
-                            {post.owner.name}
-                          </ProfileLink>
-                        </ProfileBox>
-                      </PostProfileContainer>
-                    </PostBox>
-                  ))}
-              </ImagesContainer>
-              <ImagesContainer>
-                {secondCol &&
-                  secondCol.map((post) => (
-                    <PostBox key={post?._id}>
-                      <StyledLink to={`/post/${post?.title}`} state={post?._id}>
-                        <Image
-                          src={`http://localhost:4000/${post.fileUrl[0].path}`}
-                          alt=""
-                        />
-                      </StyledLink>
-                      <PostProfileContainer>
-                        <ProfileBox>
-                          <PostProfileImg
-                            src={
-                              post.owner.profileImage
-                                ? `http://localhost:4000/${post.owner.profileImage}`
-                                : defaultUserProfileImg
-                            }
-                            alt=""
-                          />
-                          <ProfileLink
-                            to={`/user/${post.owner.username}`}
-                            state={post.owner.username}>
-                            {post.owner.name}
-                          </ProfileLink>
-                        </ProfileBox>
-                      </PostProfileContainer>
-                    </PostBox>
-                  ))}
-              </ImagesContainer>
-              <ImagesContainer>
-                {thirdCol &&
-                  thirdCol.map((post) => (
-                    <PostBox key={post?._id}>
-                      <StyledLink to={`/post/${post?.title}`} state={post?._id}>
-                        <Image
-                          src={`http://localhost:4000/${post.fileUrl[0].path}`}
-                          alt=""
-                        />
-                      </StyledLink>
-                      <PostProfileContainer>
-                        <ProfileBox>
-                          <PostProfileImg
-                            src={
-                              post.owner.profileImage
-                                ? `http://localhost:4000/${post.owner.profileImage}`
-                                : defaultUserProfileImg
-                            }
-                            alt=""
-                          />
-                          <ProfileLink
-                            to={`/user/${post.owner.username}`}
-                            state={post.owner.username}>
-                            {post.owner.name}
-                          </ProfileLink>
-                        </ProfileBox>
-                      </PostProfileContainer>
-                    </PostBox>
-                  ))}
-              </ImagesContainer>
-            </ColumnsContainer>
-          </PostsBox>
-        </PostsContainer>
-      </Box>
-    </Container>
+                          </StyledLink>
+                          <PostProfileContainer>
+                            <ProfileBox>
+                              <ProfileLink
+                                to={`/user/${post.owner.username}`}
+                                state={post.owner.username}>
+                                <PostProfileImg
+                                  src={
+                                    post.owner.profileImage
+                                      ? `http://localhost:4000/${post.owner.profileImage}`
+                                      : defaultUserProfileImg
+                                  }
+                                  alt=""
+                                />
+                                {post.owner.name}
+                              </ProfileLink>
+                            </ProfileBox>
+                          </PostProfileContainer>
+                        </PostBox>
+                      ))}
+                  </ImagesContainer>
+                  <ImagesContainer>
+                    {secondCol &&
+                      secondCol.map((post) => (
+                        <PostBox key={post?._id}>
+                          <StyledLink
+                            to={`/post/${post?.title}`}
+                            state={post?._id}>
+                            <Image
+                              src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                              alt=""
+                            />
+                          </StyledLink>
+                          <PostProfileContainer>
+                            <ProfileBox>
+                              <PostProfileImg
+                                src={
+                                  post.owner.profileImage
+                                    ? `http://localhost:4000/${post.owner.profileImage}`
+                                    : defaultUserProfileImg
+                                }
+                                alt=""
+                              />
+                              <ProfileLink
+                                to={`/user/${post.owner.username}`}
+                                state={post.owner.username}>
+                                {post.owner.name}
+                              </ProfileLink>
+                            </ProfileBox>
+                          </PostProfileContainer>
+                        </PostBox>
+                      ))}
+                  </ImagesContainer>
+                  <ImagesContainer>
+                    {thirdCol &&
+                      thirdCol.map((post) => (
+                        <PostBox key={post?._id}>
+                          <StyledLink
+                            to={`/post/${post?.title}`}
+                            state={post?._id}>
+                            <Image
+                              src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                              alt=""
+                            />
+                          </StyledLink>
+                          <PostProfileContainer>
+                            <ProfileBox>
+                              <PostProfileImg
+                                src={
+                                  post.owner.profileImage
+                                    ? `http://localhost:4000/${post.owner.profileImage}`
+                                    : defaultUserProfileImg
+                                }
+                                alt=""
+                              />
+                              <ProfileLink
+                                to={`/user/${post.owner.username}`}
+                                state={post.owner.username}>
+                                {post.owner.name}
+                              </ProfileLink>
+                            </ProfileBox>
+                          </PostProfileContainer>
+                        </PostBox>
+                      ))}
+                  </ImagesContainer>
+                </ColumnsContainer>
+              </PostsBox>
+            </PostsContainer>
+          </Box>
+        </Container>
+      ) : (
+        <Container>
+          <Header />
+          <NoSearchResults posts={posts} searchWord={searchWord} />
+        </Container>
+      )}
+    </>
   );
 }
 
