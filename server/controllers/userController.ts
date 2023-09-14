f5706b308a95e69a75a89f6e4ced50da2086be29
Min <esspacetoM@gmail.com>
2023-09-14 22:48:33 +0900

    feat: see 핸들러에서 유저의 게시글, 좋아요 목록 전달

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,19 +1,33 @@
 import { Request, Response } from "express";
 import { CustomSession } from "../types/session";
 import User from "../models/User";
+import Post from "../models/Post";
 
 export const see = async (req: Request, res: Response) => {
   const username = req.params.uid;
-  const user = await User.findOne({ username });
-  const userPosts = await user?.populate({
-    path: "posts",
-    populate: {
-      path: "owner",
-      model: "User",
-    },
+  const user = await User.findOne({ username })
+    .select("name username profileImage")
+    .populate({
+      path: "posts",
+      select: "title description fileUrl owner views likes createdAt",
+      populate: {
+        path: "owner",
+        model: "User",
+        select: "name username profileImage",
+      },
+    });
+
+  const userId = user?._id;
+  const userLiked = await Post.find({ likes: userId }).populate({
+    path: "owner",
+    select: "name username profileImage",
   });
 
-  return res.json({ profileImg: user?.profileImage, userPosts });
+  return res.json({
+    profileImg: user?.profileImage,
+    likedPosts: userLiked,
+    userPosts: user,
+  });
 };
 
 export const logout = (req: Request, res: Response) => {
