ca1fe131ea6514f2d55920554015bbf7b37b1f85
Min <esspacetoM@gmail.com>
2023-09-19 00:59:19 +0900

    design: 이미지 위에 hover 시, opacity를 주도록 설정

--- client/src/components/TopViewsPosts.tsx
+++ client/src/components/TopViewsPosts.tsx
@@ -24,7 +24,7 @@ const Text = styled.span`
   margin-top: 75px;
   font-family: "NanumGothicBold";
   font-weight: 600;
-  font-size: 20px;
+  font-size: 28px;
   margin-bottom: 15px;
 `;
 
@@ -41,6 +41,10 @@ const Image = styled.img`
   height: 300px;
   object-fit: contain;
   padding: 0px 10px;
+
+  &:hover {
+    opacity: 0.875;
+  }
 `;
 
 const sliderSettings = {
@@ -67,6 +71,10 @@ function TopViewsPosts() {
     <Container>
       <Line />
       <Text>Top 20</Text>
+      <p>
+        사용자들이 가장 많이 조회한 20개의 사진 목록이에요. 구경해 보는 건
+        어떨까요?
+      </p>
       <StyledSlider {...sliderSettings}>
         {Array.isArray(data) &&
           data?.map((post: any, index: any) => (
