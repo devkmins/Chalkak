fc7b29d4be6eda03bfc7c200af10d0677f9f6e2b
Min <esspacetoM@gmail.com>
2023-09-19 23:15:22 +0900

    edit: imagesUpload 핸들러를 req.files만 클라이언트에게 전달하도록 수정

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -20,53 +20,10 @@ export const watch = async (req: Request, res: Response) => {
   return res.status(200).json(post);
 };
 
-export const postUpload = async (req: Request, res: Response) => {
-  const session = req.session as CustomSession;
-  const userId = session.user?._id;
-  const { title, description, hashtags } = req.body.formData;
-  const postId = req.body.postId;
-  const post = await Post.findById(postId);
-
-  if (post?.owner.toString() !== userId) {
-    return res.status(403).send({ error: "권한이 없습니다." });
-  }
-
-  await Post.findByIdAndUpdate(postId, {
-    title,
-    description,
-    hashtags,
-  });
-
-  return res.status(200).json();
-};
-
 export const imagesUpload = async (req: Request, res: Response) => {
-  const session = req.session as CustomSession;
-  const userId = session.user?._id;
+  const files = req.files;
 
-  try {
-    const newPost = await Post.create({
-      title: "Default",
-      description: "Default",
-      fileUrl: req.files,
-      hashtags: [],
-      owner: userId,
-    });
-
-    await newPost.save();
-
-    const user = await User.findById(userId);
-
-    if (user) {
-      user?.posts.push(newPost._id);
-      await user?.save();
-    }
-
-    return res.status(200).json(newPost._id);
-  } catch (error) {
-    console.log(error);
-    return res.status(400).send("404");
-  }
+  return res.status(200).json(files);
 };
 
 export const postEdit = async (req: Request, res: Response) => {
