6044488c9e0aa8e2148b62ed936b57b58d783cbf
Min <esspacetoM@gmail.com>
2023-09-22 23:59:01 +0900

    feat: /post/similarPosts 경로에 라우트 추가

--- server/routes/postRouter.ts
+++ server/routes/postRouter.ts
@@ -7,6 +7,7 @@ import {
   imagesUpload,
   postViews,
   postLikes,
+  similarPosts,
 } from "../controllers/postController";
 import { imageUpload, protectorMiddleware } from "../middlewares";
 
@@ -23,5 +24,6 @@ postRouter.put("/:pid([0-9a-f]{24})/edit", postEdit);
 postRouter.put("/:pid([0-9a-f]{24})/views", postViews);
 postRouter.put("/:pid([0-9a-f]{24})/likes", postLikes);
 postRouter.delete("/:pid([0-9a-f]{24})/delete", postDelete);
+postRouter.get("/similarPosts", similarPosts);
 
 export default postRouter;
