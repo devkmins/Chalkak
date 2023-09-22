321089991aac330eef119ee2bdcdfd9133f8be37
Min <esspacetoM@gmail.com>
2023-09-22 15:49:31 +0900

    feat: 로그아웃 시에 NotificationBar 컴포넌트 렌더링

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -2,10 +2,18 @@ import Posts from "../../components/Posts";
 import Header from "../Header";
 import IntroContent from "../../components/IntroContent";
 import TopViewsPosts from "../../components/TopViewsPosts";
+import NotificationBar from "../../components/NotificationBar";
+import { useRecoilState } from "recoil";
+import { isLoggedOutState } from "../../atoms";
 
 function Main() {
+  const [isLoggedOut, setIsLoggedOut] = useRecoilState(isLoggedOutState);
+
+  const logoutText = "로그아웃이 완료되었습니다.";
+
   return (
     <>
+      {isLoggedOut && <NotificationBar text={logoutText} />}
       <Header />
       <IntroContent />
       <TopViewsPosts />
