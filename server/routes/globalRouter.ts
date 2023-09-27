3e68dc214c58a11fdb8ee156a13f16ff3cddb683
Min <esspacetoM@gmail.com>
2023-09-27 23:56:34 +0900

    edit: / 경로를 /posts로 수정

--- server/routes/globalRouter.ts
+++ server/routes/globalRouter.ts
@@ -11,7 +11,7 @@ import { publicOnlyMiddleware } from "../middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", home);
+globalRouter.get("/posts", home);
 globalRouter.post("/join", postJoin);
 globalRouter.route("/login").get(getLogin).post(postLogin);
 globalRouter.get("/search/:keyword", search);
