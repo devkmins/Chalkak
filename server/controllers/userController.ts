8d19bfce6e080abc2f0abd88dcf1570cb6bf723f
Min <esspacetoM@gmail.com>
2023-09-29 23:54:22 +0900

    feat: 페이지에 따라 유저의 게시글과 좋아요한 게시글을 전달하도록 설정

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -5,29 +5,46 @@ import Post from "../models/Post";
 
 export const see = async (req: Request, res: Response) => {
   const username = req.params.uid;
-  const user = await User.findOne({ username })
-    .select("name username profileImage")
-    .populate({
-      path: "posts",
-      select: "title description fileUrl owner views likes createdAt",
-      populate: {
+  let page = Number(req.query.page) || 1;
+  const perPage = 10;
+
+  try {
+    const user = await User.findOne({ username })
+      .select("name username profileImage")
+      .populate({
+        path: "posts",
+        select: "title description fileUrl owner views likes createdAt",
+        options: {
+          limit: perPage * page,
+        },
+        populate: {
+          path: "owner",
+          model: "User",
+          select: "name username profileImage",
+        },
+      });
+
+    if (!user) {
+      return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+    }
+
+    const userId = user._id;
+    const userLiked = await Post.find({ likes: userId })
+      .limit(perPage * page)
+      .populate({
         path: "owner",
-        model: "User",
         select: "name username profileImage",
-      },
+      });
+
+    return res.json({
+      profileImg: user.profileImage,
+      likedPosts: userLiked,
+      userPosts: user,
     });
-
-  const userId = user?._id;
-  const userLiked = await Post.find({ likes: userId }).populate({
-    path: "owner",
-    select: "name username profileImage",
-  });
-
-  return res.json({
-    profileImg: user?.profileImage,
-    likedPosts: userLiked,
-    userPosts: user,
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "서버 오류" });
+  }
 };
 
 export const logout = (req: Request, res: Response) => {
