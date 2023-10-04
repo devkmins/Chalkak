fcb3b85473d4630699aa4682738d18305e1ab472
Min <esspacetoM@gmail.com>
2023-10-04 20:44:27 +0900

    feat: 이미지의 비율을 구하고 반환하도록 설정

--- client/src/resizeAndConvertToWebP.ts
+++ client/src/resizeAndConvertToWebP.ts
@@ -1,5 +1,13 @@
 import Resizer from "react-image-file-resizer";
 
+function gcd(a: number, b: number): number {
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+
 export async function resizeAndConvertToWebP(imageFile: File) {
   return new Promise(async (resolve, reject) => {
     const image = new Image();
@@ -8,6 +16,9 @@ export async function resizeAndConvertToWebP(imageFile: File) {
     image.onload = () => {
       const originalWidth = image.width;
       const originalHeight = image.height;
+      const gcdValue = gcd(originalWidth, originalHeight);
+      const ratioWidth = originalWidth / gcdValue;
+      const ratioHeight = originalHeight / gcdValue;
 
       Resizer.imageFileResizer(
         imageFile,
@@ -17,7 +28,12 @@ export async function resizeAndConvertToWebP(imageFile: File) {
         50,
         0,
         (blob) => {
-          resolve(blob);
+          const result = {
+            blob,
+            ratioWidth,
+            ratioHeight,
+          };
+          resolve(result);
         },
         "file"
       );
