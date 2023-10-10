331bd498aca32b7663bffa9fe5553aafbcc16aea
Min <esspacetoM@gmail.com>
2023-10-10 23:58:25 +0900

    design: SimilarPosts 반응형

--- client/src/components/SimilarPosts.tsx
+++ client/src/components/SimilarPosts.tsx
@@ -11,29 +11,31 @@ import {
   isBackToSimilarPostsState,
   similarPostsScrollYState,
 } from "../atoms";
+import { useDesktop } from "../styles/mediaQueries";
 
 interface IProp {
   title: string;
   postId: any;
 }
 
+interface IColumnsContainerProps {
+  $isDesktop: string;
+}
+
 const Container = styled.div`
   width: 100%;
 `;
 
-const ColumnsContainer = styled.div`
+const ColumnsContainer = styled.div<IColumnsContainerProps>`
   display: grid;
-  grid-template-columns: repeat(3, 31.5%);
+  grid-template-columns: ${(props) =>
+    props.$isDesktop === "true" ? "repeat(3, 31.5%)" : "repeat(2, 47.5%)"};
   grid-auto-rows: auto;
   grid-gap: 25px;
   justify-content: center;
   padding: 0px 15px;
   padding-top: 35px;
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-    grid-auto-rows: auto;
-  }
+  width: 100%;
 `;
 
 const ImagesContainer = styled.div``;
@@ -98,6 +100,8 @@ const ImagesBox = styled.div`
 `;
 
 function SimilarPosts({ title, postId }: IProp) {
+  const isDesktop = useDesktop();
+
   const location = useLocation();
   const path = location.pathname;
 
@@ -141,26 +145,40 @@ function SimilarPosts({ title, postId }: IProp) {
   const [thirdCol, setThirdCol] = useState<any[]>([]);
 
   useEffect(() => {
-    const firstColImages: string[] = [];
-    const secondColImages: string[] = [];
-    const thirdColImages: string[] = [];
+    const firstColImages: any[] = [];
+    const secondColImages: any[] = [];
+    const thirdColImages: any[] = [];
 
-    if (data && Array.isArray(data)) {
-      data?.forEach((post: any, index: any) => {
-        if (index % 3 === 0) {
-          firstColImages.push(post);
-        } else if (index % 3 === 1) {
-          secondColImages.push(post);
-        } else if (index % 3 === 2) {
-          thirdColImages.push(post);
-        }
-      });
+    if (!isDesktop) {
+      if (data && Array.isArray(data)) {
+        data?.forEach((post: any, index: any) => {
+          if (index % 2 === 0) {
+            firstColImages.push(post);
+          } else if (index % 2 === 1) {
+            secondColImages.push(post);
+          }
+        });
+      }
+    }
+
+    if (isDesktop) {
+      if (data && Array.isArray(data)) {
+        data?.forEach((post: any, index: any) => {
+          if (index % 3 === 0) {
+            firstColImages.push(post);
+          } else if (index % 3 === 1) {
+            secondColImages.push(post);
+          } else if (index % 3 === 2) {
+            thirdColImages.push(post);
+          }
+        });
+      }
     }
 
     setFirstCol(firstColImages);
     setSecondCol(secondColImages);
     setThirdCol(thirdColImages);
-  }, [data]);
+  }, [data, isDesktop]);
 
   const [scrollY, setScrollY] = useRecoilState(similarPostsScrollYState);
 
@@ -190,110 +208,184 @@ function SimilarPosts({ title, postId }: IProp) {
 
   return (
     <Container>
-      <ColumnsContainer>
-        <ImagesContainer>
-          {firstCol &&
-            firstCol.map((post) => (
-              <ImagesBox key={post?._id}>
-                <StyledLink
-                  to={`/post/${post?.title}`}
-                  state={{ postId: post?._id, path }}
-                  onClick={clickedPost}>
-                  <Image
-                    src={`http://localhost:4000/${post.fileUrl[0].path}`}
-                    alt=""
-                  />
-                </StyledLink>
-                <ProfileContainer>
-                  <ProfileBox>
-                    <ProfileLink
-                      to={`/user/${post.owner.username}`}
-                      state={path}
-                      onClick={clickedProfile}>
-                      <ProfileImg
-                        src={
-                          post.owner.profileImage
-                            ? `http://localhost:4000/${post.owner.profileImage}`
-                            : defaultUserProfileImg
-                        }
-                        alt=""
-                      />
-                      {post.owner.name}
-                    </ProfileLink>
-                  </ProfileBox>
-                </ProfileContainer>
-              </ImagesBox>
-            ))}
-        </ImagesContainer>
-        <ImagesContainer>
-          {secondCol &&
-            secondCol.map((post) => (
-              <ImagesBox key={post?._id}>
-                <StyledLink
-                  to={`/post/${post?.title}`}
-                  state={{ postId: post?._id, path }}
-                  onClick={clickedPost}>
-                  <Image
-                    src={`http://localhost:4000/${post.fileUrl[0].path}`}
-                    alt=""
-                  />
-                </StyledLink>
-                <ProfileContainer>
-                  <ProfileBox>
-                    <ProfileLink
-                      to={`/user/${post.owner.username}`}
-                      state={path}
-                      onClick={clickedProfile}>
-                      <ProfileImg
-                        src={
-                          post.owner.profileImage
-                            ? `http://localhost:4000/${post.owner.profileImage}`
-                            : defaultUserProfileImg
-                        }
-                        alt=""
-                      />
-                      {post.owner.name}
-                    </ProfileLink>
-                  </ProfileBox>
-                </ProfileContainer>
-              </ImagesBox>
-            ))}
-        </ImagesContainer>
-        <ImagesContainer>
-          {thirdCol &&
-            thirdCol.map((post) => (
-              <ImagesBox key={post?._id}>
-                <StyledLink
-                  to={`/post/${post?.title}`}
-                  state={{ postId: post?._id, path }}
-                  onClick={clickedPost}>
-                  <Image
-                    src={`http://localhost:4000/${post.fileUrl[0].path}`}
-                    alt=""
-                  />
-                </StyledLink>
-                <ProfileContainer>
-                  <ProfileBox>
-                    <ProfileLink
-                      to={`/user/${post.owner.username}`}
-                      state={path}
-                      onClick={clickedProfile}>
-                      <ProfileImg
-                        src={
-                          post.owner.profileImage
-                            ? `http://localhost:4000/${post.owner.profileImage}`
-                            : defaultUserProfileImg
-                        }
-                        alt=""
-                      />
-                      {post.owner.name}
-                    </ProfileLink>
-                  </ProfileBox>
-                </ProfileContainer>
-              </ImagesBox>
-            ))}
-        </ImagesContainer>
-      </ColumnsContainer>
+      {!isDesktop && (
+        <ColumnsContainer $isDesktop={String(isDesktop)}>
+          <ImagesContainer>
+            {firstCol &&
+              firstCol.map((post) => (
+                <ImagesBox key={post?._id}>
+                  <StyledLink
+                    to={`/post/${post?.title}`}
+                    state={{ postId: post?._id, path }}
+                    onClick={clickedPost}>
+                    <Image
+                      src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                      alt=""
+                    />
+                  </StyledLink>
+                  <ProfileContainer>
+                    <ProfileBox>
+                      <ProfileLink
+                        to={`/user/${post.owner.username}`}
+                        state={path}
+                        onClick={clickedProfile}>
+                        <ProfileImg
+                          src={
+                            post.owner.profileImage
+                              ? `http://localhost:4000/${post.owner.profileImage}`
+                              : defaultUserProfileImg
+                          }
+                          alt=""
+                        />
+                        {post.owner.name}
+                      </ProfileLink>
+                    </ProfileBox>
+                  </ProfileContainer>
+                </ImagesBox>
+              ))}
+          </ImagesContainer>
+          <ImagesContainer>
+            {secondCol &&
+              secondCol.map((post) => (
+                <ImagesBox key={post?._id}>
+                  <StyledLink
+                    to={`/post/${post?.title}`}
+                    state={{ postId: post?._id, path }}
+                    onClick={clickedPost}>
+                    <Image
+                      src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                      alt=""
+                    />
+                  </StyledLink>
+                  <ProfileContainer>
+                    <ProfileBox>
+                      <ProfileLink
+                        to={`/user/${post.owner.username}`}
+                        state={path}
+                        onClick={clickedProfile}>
+                        <ProfileImg
+                          src={
+                            post.owner.profileImage
+                              ? `http://localhost:4000/${post.owner.profileImage}`
+                              : defaultUserProfileImg
+                          }
+                          alt=""
+                        />
+                        {post.owner.name}
+                      </ProfileLink>
+                    </ProfileBox>
+                  </ProfileContainer>
+                </ImagesBox>
+              ))}
+          </ImagesContainer>
+        </ColumnsContainer>
+      )}
+      {isDesktop && (
+        <ColumnsContainer $isDesktop={String(isDesktop)}>
+          <ImagesContainer>
+            {firstCol &&
+              firstCol.map((post) => (
+                <ImagesBox key={post?._id}>
+                  <StyledLink
+                    to={`/post/${post?.title}`}
+                    state={{ postId: post?._id, path }}
+                    onClick={clickedPost}>
+                    <Image
+                      src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                      alt=""
+                    />
+                  </StyledLink>
+                  <ProfileContainer>
+                    <ProfileBox>
+                      <ProfileLink
+                        to={`/user/${post.owner.username}`}
+                        state={path}
+                        onClick={clickedProfile}>
+                        <ProfileImg
+                          src={
+                            post.owner.profileImage
+                              ? `http://localhost:4000/${post.owner.profileImage}`
+                              : defaultUserProfileImg
+                          }
+                          alt=""
+                        />
+                        {post.owner.name}
+                      </ProfileLink>
+                    </ProfileBox>
+                  </ProfileContainer>
+                </ImagesBox>
+              ))}
+          </ImagesContainer>
+          <ImagesContainer>
+            {secondCol &&
+              secondCol.map((post) => (
+                <ImagesBox key={post?._id}>
+                  <StyledLink
+                    to={`/post/${post?.title}`}
+                    state={{ postId: post?._id, path }}
+                    onClick={clickedPost}>
+                    <Image
+                      src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                      alt=""
+                    />
+                  </StyledLink>
+                  <ProfileContainer>
+                    <ProfileBox>
+                      <ProfileLink
+                        to={`/user/${post.owner.username}`}
+                        state={path}
+                        onClick={clickedProfile}>
+                        <ProfileImg
+                          src={
+                            post.owner.profileImage
+                              ? `http://localhost:4000/${post.owner.profileImage}`
+                              : defaultUserProfileImg
+                          }
+                          alt=""
+                        />
+                        {post.owner.name}
+                      </ProfileLink>
+                    </ProfileBox>
+                  </ProfileContainer>
+                </ImagesBox>
+              ))}
+          </ImagesContainer>
+          <ImagesContainer>
+            {thirdCol &&
+              thirdCol.map((post) => (
+                <ImagesBox key={post?._id}>
+                  <StyledLink
+                    to={`/post/${post?.title}`}
+                    state={{ postId: post?._id, path }}
+                    onClick={clickedPost}>
+                    <Image
+                      src={`http://localhost:4000/${post.fileUrl[0].path}`}
+                      alt=""
+                    />
+                  </StyledLink>
+                  <ProfileContainer>
+                    <ProfileBox>
+                      <ProfileLink
+                        to={`/user/${post.owner.username}`}
+                        state={path}
+                        onClick={clickedProfile}>
+                        <ProfileImg
+                          src={
+                            post.owner.profileImage
+                              ? `http://localhost:4000/${post.owner.profileImage}`
+                              : defaultUserProfileImg
+                          }
+                          alt=""
+                        />
+                        {post.owner.name}
+                      </ProfileLink>
+                    </ProfileBox>
+                  </ProfileContainer>
+                </ImagesBox>
+              ))}
+          </ImagesContainer>
+        </ColumnsContainer>
+      )}
     </Container>
   );
 }
