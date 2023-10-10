22fcae79a227acb57851342e363194b3acf8c57b
Min <esspacetoM@gmail.com>
2023-10-10 23:14:13 +0900

    edit: mobile, tablet width 수정

--- client/src/styles/mediaQueries.ts
+++ client/src/styles/mediaQueries.ts
@@ -2,7 +2,7 @@ import { useMediaQuery } from "react-responsive";
 
 export const useSmallDevice = () =>
   useMediaQuery({ query: "(max-width: 575px)" });
-export const useMobile = () => useMediaQuery({ query: "(max-width: 769px)" });
+export const useMobile = () => useMediaQuery({ query: "(max-width: 674px)" });
 export const useTabletOrLaptop = () =>
-  useMediaQuery({ query: "(min-width: 770px) and (max-width: 1169px)" });
+  useMediaQuery({ query: "(min-width: 675px) and (max-width: 1169px)" });
 export const useDesktop = () => useMediaQuery({ query: "(min-width: 1170px)" });
