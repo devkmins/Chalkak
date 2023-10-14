051e70cee934df2a0554dc41d9a63f9e3f161dfc
Min <esspacetoM@gmail.com>
2023-10-15 03:21:28 +0900

    refactor: error messages 설정

---
+++ client/src/constants/errorMessages.ts
@@ -0,0 +1,23 @@
+export const passwordWhiteSpaceError =
+  "비밀번호에는 공백이 포함되어서는 안 됩니다.";
+
+export const passwordLengthError = "비밀번호는 8자 이상 16자 이하여야 합니다.";
+
+export const passwordComplexityError =
+  "비밀번호는 1개 이상의 숫자, 특수문자가 포함되어야 합니다.";
+
+export const usernameLengthError =
+  "사용자 이름은 4자 이상 20자 이하여야 합니다.";
+
+export const usernameWhiteSpaceError =
+  "사용자 이름에는 공백이 포함되어서는 안 됩니다.";
+
+export const emailValidityError = "이메일이 유효하지 않습니다.";
+
+export const nameLengthError = "이름은 2자 이상 20자 이하여야 합니다.";
+
+export const nameWhiteSpaceError =
+  "이름의 첫 글자, 마지막 글자에는 공백이 포함 되어서는 안 됩니다.";
+
+export const loginFailureLimitExceededError =
+  "로그인 실패 횟수가 10회를 초과하였습니다. 5분 후에 다시 시도해주세요.";
