3f1abe062ed2e349256dd33f6051aa3b241b8f5d
Min <esspacetoM@gmail.com>
2023-10-15 01:55:54 +0900

    refactor: notification messages 설정

---
+++ client/src/constants/notificationMessages.ts
@@ -0,0 +1,11 @@
+export const passwordChangedText = "비밀번호가 변경되었습니다.";
+
+export const photoUpdateSuccessText = "사진이 업데이트 되었습니다.";
+
+export const accountUpdatedText = "계정이 업데이트 되었습니다.";
+
+export const logoutSuccessText = "로그아웃이 완료되었습니다.";
+
+export const getJoinSuccessText = (userName: string) => {
+  return `안녕하세요 ${userName}님, 회원가입이 완료되었어요!`;
+};
