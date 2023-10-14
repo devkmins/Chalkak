885d16ace1913d2fa6866b5899c41f2a6e486348
Min <esspacetoM@gmail.com>
2023-10-15 04:53:28 +0900

    refactor: 경로 설정

---
+++ client/src/constants/paths.ts
@@ -0,0 +1,25 @@
+export const MAIN_PATH = "/";
+
+export const LOGIN_PATH = "/login";
+
+export const JOIN_PATH = "/join";
+
+export const ACCOUNT_PATH = "/account";
+
+export const CHANGE_PASSWORD_PATH = "/account/password";
+
+export const CLOSE_ACCOUNT_PATH = "/account/close";
+
+export const SEARCH_PATH = "/search";
+
+export const USER_PATH = "/user";
+
+export const USER_PROFILE_PATH = "/user/:id";
+
+export const USER_LIKES_PATH = "/user/:id/likes";
+
+export const POST_PATH = "/post";
+
+export const POST_UPLOAD_PATH = "/post/upload";
+
+export const POST_DETAIL_PATH = "/post/:id";
