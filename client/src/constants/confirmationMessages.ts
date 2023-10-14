06069050b58931a8282797b383459b687af6f671
Min <esspacetoM@gmail.com>
2023-10-15 01:55:44 +0900

    refactor: confirm messages 설정

---
+++ client/src/constants/confirmationMessages.ts
@@ -0,0 +1,6 @@
+export const accountClosureConfirmation = "계정을 정말 폐쇄하실 건가요?";
+
+export const photoRemovalConfirmation = "사진을 정말 제거하실 건가요?";
+
+export const closeConfirmationMessage =
+  "모든 업로드가 취소됩니다. 정말로 닫으시겠습니까?";
