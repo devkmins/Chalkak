a9371087f52af18fd9eace83189581573f0786da
Min <esspacetoM@gmail.com>
2023-10-17 04:17:56 +0900

    refactor: react query 키들을 모아서 reactQueryKeys 파일 내에 저장

---
+++ client/src/constants/reactQueryKeys.ts
@@ -0,0 +1,9 @@
+export const ALL_POSTS_DATA = "getAllPostsData";
+
+export const DETAIL_POST_DATA = "getPostDetailData";
+
+export const EDIT_POST_DATA = "getPostEditData";
+
+export const USER_PROFILE_DATA = "getUserProfileData";
+
+export const SIMILAR_POSTS_DATA = "getSimilarPostsData";
