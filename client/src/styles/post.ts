978eca085eb7e48e110c142430ec34be099e144d
Min <esspacetoM@gmail.com>
2023-10-17 18:17:17 +0900

    refactor: 이미지를 렌더링하는 부분의 스타일을 post 파일에 저장

---
+++ client/src/styles/post.ts
@@ -0,0 +1,84 @@
+// Library
+import { Link } from "react-router-dom";
+
+// Package
+import styled from "styled-components";
+
+// Types
+import { IIsMobile } from "../types/mediaQueriesType";
+import { IRatioTypes } from "../types/ratioType";
+
+export const ImagesContainer = styled.div``;
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  width: 100%;
+`;
+
+export const Image = styled.img<IRatioTypes>`
+  width: 100%;
+  max-height: max-content;
+  aspect-ratio: ${(props) => props.$ratioWidth} /
+    ${(props) => props.$ratioHeight};
+`;
+
+export const ProfileContainer = styled.div<IIsMobile>`
+  display: none;
+  position: absolute;
+  bottom: 0;
+  width: max-content;
+  height: max-content;
+  margin-bottom: 17.5px;
+
+  ${(props) =>
+    props.$isMobile === "true" &&
+    `
+    display: flex;
+    position: static;
+    margin-bottom: 12.5px;
+    `}
+`;
+
+export const ProfileBox = styled.div<IIsMobile>`
+  display: flex;
+  align-items: center;
+  padding: 0px ${(props) => (props.$isMobile === "true" ? "12.5px" : "20px")};
+`;
+
+export const ProfileLink = styled(Link)<IIsMobile>`
+  display: flex;
+  align-items: center;
+  color: ${(props) => (props.$isMobile === "true" ? "black" : "#e0dfdf")};
+  font-size: 16px;
+
+  ${(props) =>
+    props.$isMobile === "false" &&
+    `
+    &:hover {
+      color: white;
+      transition: color 0.25s;
+    }
+  `}
+`;
+
+export const PostProfileImg = styled.img`
+  border-radius: 50%;
+  margin-right: 10px;
+  width: 32.5px;
+  height: 32.5px;
+`;
+
+export const PostBox = styled.div<IIsMobile>`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin-bottom: ${(props) => (props.$isMobile === "true" ? "50px" : "25px")};
+
+  &:hover {
+    opacity: ${(props) => (props.$isMobile === "true" ? "1" : "0.875")};
+
+    ${ProfileContainer} {
+      display: flex;
+    }
+  }
+`;
