485c3b0b5c89a5e92f7bbef250cb43dc1910506b
Min <esspacetoM@gmail.com>
2023-10-17 18:52:52 +0900

    refactor: 색깔들을 colors에 저장

---
+++ client/src/constants/colors.ts
@@ -0,0 +1,33 @@
+export const WHITE_COLOR = "white";
+
+export const BLACK_COLOR = "black";
+
+export const PROFILE_LINK_COLOR = "#e0dfdf";
+
+export const ACCOUNT_MENU_LINK_COLOR = "#060606";
+
+export const LIGHT_GRAY_COLOR = "#dddddd";
+
+export const ERROR_MESSAGE_COLOR = "#ff6b6b";
+
+export const INPUT_BOX_FOCUS_COLOR = "#111111";
+
+export const DELETE_POST_TEXT_BOX_COLOR = "#fdf6f4";
+
+export const DELETE_POST_BTN_COLOR = "#df4927";
+
+export const BS_THREE_DOTS_COLOR = "#8c8888";
+
+export const GR_FORM_CLOSE_COLOR = "#f2efef";
+
+export const A_LITTLE_DARK_GRAY_COLOR = "#4d4d4d";
+
+export const NORMAL_GRAY_COLOR = "#767676";
+
+export const A_LITTILE_LIGHT_GRAY_COLOR = "#c8d6e5";
+
+export const HEART_CLICKED_COLOR = "#ff6b6b";
+
+export const HEART_UNCLICKED_COLOR = "#576574";
+
+export const GRAY_COLOR = "gray";
